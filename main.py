fd1777778bfd514109f79ef7bcc41d985d2b514d
陈树德 <935827122@qq.com>
2020-07-29 17:23:11 +0800

    ①读取PCR数据文件，解包后的spine动画信息文件 ②输出为模拟器必要的data.json文件

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ for key in data.unit_list:
     unit = data.unit_list[key]
     unit['promotion'] = unit['promotion'].tolist()
     unit['atk_pattern'] = unit['atk_pattern'].tolist()
-with open("data.json", "w",encoding='utf-8')as f:
+with open("data.json", "w", encoding='utf-8')as f:
     f.write(json.dumps(data.unit_list,ensure_ascii=False, indent=4, separators=(',', ': ')))
